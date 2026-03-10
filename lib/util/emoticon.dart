80e1ceb707e389c74c577df869d08305aa1f11de
Nguyen Kieu Trang <hoangbaotrang191105@gmail.com>
2026-03-10 13:30:35 +0700

    sửa click trang home

--- lib/util/emoticon.dart
+++ lib/util/emoticon.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
 
-class CamXuc extends StatelessWidget {
+class CamXuc extends StatefulWidget {
 
   final String camxuc;
+
   const CamXuc({
     Key? key,
     required this.camxuc,
   }) : super(key: key);
 
   @override
+  State<CamXuc> createState() => _CamXucState();
+}
+
+class _CamXucState extends State<CamXuc> {
+
+  bool dangClick = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.blue[500],
-        borderRadius: BorderRadius.circular(12),
-      ),
-      padding: EdgeInsets.all(12),
-      child: Text(
-        camxuc,
-        style: TextStyle(
-          fontSize: 30,
+    return GestureDetector(
+
+      onTap: (){
+        setState(() {
+          dangClick = !dangClick;
+        });
+      },
+
+      child: Container(
+        decoration: BoxDecoration(
+          // color: Colors.blue[500],
+          color: dangClick ? Colors.orange : Colors.blue,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: EdgeInsets.all(12),
+        child: Text(
+          widget.camxuc,
+          style: TextStyle(
+            fontSize: 30,
+          ),
         ),
       ),
     );
